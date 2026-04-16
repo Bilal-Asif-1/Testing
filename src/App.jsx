f68d60cbbcacd7c32b75371f83c532ba392c8e85
Bilal Asif <bilalasif@Bilals-MacBook-Pro.local>
2026-04-16 17:28:31 +0500

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import Description from './Description.jsx';
-import './App.css';
+// import Description from './Description.jsx';
+// import './App.css';
+
+// function App() {
+//   let feature = ['good', 'bad', 'poor'];
+
+//   return (
+//     <div className="style">
+//       <Description
+//         desc="HI I AM BILAL"
+//         price={3000}
+//         nam={{ a: 'bilal' }}
+//         feat={feature}
+//       />
+//       <Description
+//         desc="HI I AM BILAL"
+//         price={5000}
+//         nam={{ a: 'bilal' }}
+//         feat={feature}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import Description from './Description';
 
 function App() {
-  let feature = ['good', 'bad', 'poor'];
-
   return (
-    <div className="style">
-      <Description
-        desc="HI I AM BILAL"
-        price={3000}
-        nam={{ a: 'bilal' }}
-        feat={feature}
-      />
-      <Description
-        desc="HI I AM BILAL"
-        price={5000}
-        nam={{ a: 'bilal' }}
-        feat={feature}
-      />
+    <div>
+      <Description userName="bilal" color="red" />
+      <Description userName="Ahmad" color="blue" />
     </div>
   );
 }
--- src/Description.jsx
+++ src/Description.jsx
@@ -1,28 +1,27 @@
-function Description({ desc, price, feat, nam }) {
-  const list = feat.map((el) => <li key={el}>{el}</li>);
-  if (price > 4000) {
-    return (
-      <div>
-        <p>
-          {desc}
-          {price}
-          <p>Discount of 5 %</p>
-          {nam.a}
-          {list}
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <p>
-          {desc}
-          {price}
-          {nam.a}
-          {list}
-        </p>
-      </div>
-    );
-  }
+// function Description({ desc, price, feat, nam }) {
+//   const list = feat.map((el) => <li key={el}>{el}</li>);
+//   let isDiscount = price > 3000 ? '5 percent discount available' : null;
+//   let styles = { backgroundColor: price > 3000 ? 'yellow' : 'black' };
+//   return (
+//     <div>
+//       <p>
+//         {desc}
+//         <p style={styles}>{price}</p>
+//         <p>{isDiscount}</p>
+//         {nam.a}
+//         {list}
+//       </p>
+//     </div>
+//   );
+// }
+// export default Description;
+
+function Description({ userName, color }) {
+  return (
+    <div style={{ backgroundColor: color }}>
+      <p>{userName}</p>
+    </div>
+  );
 }
+
 export default Description;
