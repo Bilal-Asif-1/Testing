02be727cd48b251d8961a4e39edf436144bb67b5
Bilal Asif <bilalasif@Bilals-MacBook-Pro.local>
2026-04-16 17:28:31 +0500

    latest

--- src/Description.jsx
+++ src/Description.jsx
@@ -16,6 +16,32 @@ function Description() {
   );
 }
 
+// function Description({ desc, price, feat, nam }) {
+//   const list = feat.map((el) => <li key={el}>{el}</li>);
+//   let isDiscount = price > 3000 ? '5 percent discount available' : null;
+//   let styles = { backgroundColor: price > 3000 ? 'yellow' : 'black' };
+//   return (
+//     <div>
+//       <p>
+//         {desc}
+//         <p style={styles}>{price}</p>
+//         <p>{isDiscount}</p>
+//         {nam.a}
+//         {list}
+//       </p>
+//     </div>
+//   );
+// }
+// export default Description;
+
+function Description({ userName, color }) {
+  return (
+    <div style={{ backgroundColor: color }}>
+      <p>{userName}</p>
+    </div>
+  );
+}
+
 export default Description;
 
 // function Description({ desc, price, feat, nam }) {
