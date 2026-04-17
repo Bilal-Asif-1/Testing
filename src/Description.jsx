d541cac298514c484c3b6016bd7228fb256d3a85
Bilal Asif <bilalasif@Bilals-MacBook-Pro.local>
2026-04-17 16:59:14 +0500

    event listners

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,49 +1,3 @@
-function print() {
-  console.log('HELLOOO');
-}
-
-function hover() {
-  console.log('HOVERRRR');
-}
-
-function Description() {
-  return (
-    <div>
-      <p>
-        <button onMouseOver={hover}>Click</button>
-      </p>
-    </div>
-  );
-}
-
-// function Description({ desc, price, feat, nam }) {
-//   const list = feat.map((el) => <li key={el}>{el}</li>);
-//   let isDiscount = price > 3000 ? '5 percent discount available' : null;
-//   let styles = { backgroundColor: price > 3000 ? 'yellow' : 'black' };
-//   return (
-//     <div>
-//       <p>
-//         {desc}
-//         <p style={styles}>{price}</p>
-//         <p>{isDiscount}</p>
-//         {nam.a}
-//         {list}
-//       </p>
-//     </div>
-//   );
-// }
-// export default Description;
-
-function Description({ userName, color }) {
-  return (
-    <div style={{ backgroundColor: color }}>
-      <p>{userName}</p>
-    </div>
-  );
-}
-
-export default Description;
-
 // function Description({ desc, price, feat, nam }) {
 //   const list = feat.map((el) => <li key={el}>{el}</li>);
 //   let isDiscount = price > 3000 ? '5 percent discount available' : null;
@@ -70,4 +24,4 @@ export default Description;
 //   );
 // }
 
-// export default Description;
+export default Description;
